87c5f4e1d8786f555588ae8b5aed76f6daf2d5db
SayanDasDev <mesayan19@gmail.com>
2023-06-27 08:37:46 +0530

    header fixed

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="fixed-div" style="display: flex; border-bottom: 1px solid #000; background: linear-gradient(to right, #e91a43, #FF4081); opacity: 0.8;">
-    <div style="display: flex; width: 75%;">
-      <div style="display: flex; flex: 1; justify-content: flex-start; align-items: center; margin: 10px;">
+  <div class="fixed-div">
+      <div>
         <router-link to="/">
-          <vs-avatar size="70px" src="https://bijit.xyz/cockpit-master/storage/uploads/2023/06/24/logo_uid_64967492a4ab1.png"/>
+          <vs-avatar size="64px" src="https://bijit.xyz/cockpit-master/storage/uploads/2023/06/24/logo_uid_64967492a4ab1.png"/>
         </router-link>
       </div>
-      <div style="display: flex; flex: 2; justify-content: center; align-items: center;">
+      <div class="nav-heading-text">
         <span class="anurati logo">EUREKA</span>
       </div>
-    </div>
   </div>
 </template>
 <script>
@@ -32,4 +30,12 @@ export default {
   border-bottom: 1px solid #000;
   background: linear-gradient(to right, #e91a43, #FF4081);
   opacity: 0.8;
+  align-items: center;
+  padding: 0.5rem 2rem;
+
+.nav-heading-text
+  margin: 0 auto;
+
 </style>
+
+
