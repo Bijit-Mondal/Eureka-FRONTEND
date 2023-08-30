40aa9c1e3b0ed22706d53f3736a3e39c16c673a4
Bijit Mondal <bijitmondal3011@rediffmail.com>
2023-08-30 17:07:30 +0530

    Hint Updated

--- src/components/Question/QuestionComponent.vue
+++ src/components/Question/QuestionComponent.vue
@@ -1,15 +1,17 @@
 <template>
   <div style="margin-bottom: 6rem;">
     <div style="display: flex; justify-content: flex-end; align-items: center;">
-      <span class="badge text-color glow outfit" style="margin-top: 12vb;">Marks: {{levelStore.score}}</span>
+      <span class="badge text-color glow outfit" style="margin-top: 12vb;">Marks: 50</span>
     </div>
     <div style="display: flex; margin-top: 2.5vb; justify-content: center; align-items: center;">
       <h1 class="text-color outfit">{{levelStore.level+1}}. {{ title }}</h1>
-      <span style="width: 3em; height: 2em;">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#E91A42" class="w-6 h-6 bulb-icon">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
-        </svg>
-      </span>
+      <vs-tooltip @click="intervalGetHint" text="Get Hint" position="right" >
+        <span style="display:block; width: 3em; height: 2em; cursor: pointer;">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#E91A42" class="w-6 h-6 bulb-icon">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+          </svg>
+        </span>
+      </vs-tooltip>
     </div>
 
     <vs-row vs-justify="center">
@@ -48,6 +50,8 @@ export default {
       question: '',
       answer: '',
       levelStore: '',
+      lastExecutionTime: 0, // Initialize with a value that guarantees the method can be executed immediately
+      delayDuration: 20000, // 10 seconds in milliseconds
     }
   },
   methods: {
@@ -57,36 +61,41 @@ export default {
       this.levelStore = useLevelStore(); 
       getQuestionById(this.$route.params.id)
       .then((data)=>{
-        console.log (this.hints);
         this.imgSrc = data.imgSrc;
         this.title = data.title;
         this.question = data.question;
-        this.getHint(data.hints);
         this.$vs.loading.close();
       }).catch((err)=>{
         console.log(err);
         this.$vs.loading.close();
       })
     },
-    getHint(hints){
-      const maxHints = hints.length;
-      const intervalFunction = () => {
-        if (this.levelStore.hintUsed < maxHints) {
-          this.$vs.notify({ title: 'Hint', text: hints[this.levelStore.hintUsed], color: 'danger',time:10000});
-          this.levelStore.hintUsing();
-          this.levelStore.setScore()
-        } else {
-          clearInterval(this.interval);
-        }
-      };
-      this.interval = setInterval(intervalFunction, 10000);
+    intervalGetHint() {
+      const currentTime = new Date().getTime();
+      // console.log(`Delay ${currentTime - this.lastExecutionTime}`)
+      if (currentTime - this.lastExecutionTime >= this.delayDuration) {
+        this.getHint();
+        this.lastExecutionTime = currentTime;
+      } else {
+        // You can display a message to the user here indicating they need to wait before trying again
+        this.$vs.notify({ title: 'Waitt a bit', text: 'Please wait before getting another hint.', color: 'dark'});
+      }
+    },
+    getHint(){
+      const {getHintById} = useQuesion();
+        getHintById(this.$route.params.id,this.levelStore.hint).then((data)=>{
+          this.$vs.notify({ title: 'Hint', text: data.hint, color: 'danger',time:20000});
+          this.levelStore.hint +=1;
+        }).catch((err)=>{
+          console.log(err);
+        })
     },
     submitAnswer(){ 
       if(this.answer==''){
         this.$vs.notify({title:'Error',text:'I believe in you, you can answer', color: "dark",position: 'top-right',fixed: true});
       }
       const {scoreUpdate} = useScore();
-      scoreUpdate(this.$route.params.id,this.answer,this.levelStore.score)
+      scoreUpdate(this.$route.params.id,this.answer)
       .then((data)=>{
         if(data.msg == "Correct Answer"){
           this.$vs.notify({title:'Success',text:data.msg, color: "success",position: 'top-right',fixed: true});
@@ -99,9 +108,9 @@ export default {
       }).catch((err)=>{
         alert(err);
       })
-    }
+    },
   },
-  mounted(){
+  created(){
     this.getQuestion();
   }
 }
