d24b4622c89eb3be1a9fa761d97689e21f7d2027
Bijit Mondal <bijitmondal3011@rediffmail.com>
2023-08-31 17:19:07 +0530

    Server Updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: 'http://192.168.1.10:3000/',
+  baseURL: 'https://backend-eureka.bijit.xyz/',
   headers: {
     'Content-Type': 'application/json',
   },
