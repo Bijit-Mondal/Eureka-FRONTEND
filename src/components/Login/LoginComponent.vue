ece0823c519ad58b100729a2d86156375f79d402
Bijit Mondal <68532969+Bijit-Mondal@users.noreply.github.com>
2023-10-04 00:50:43 +0530

    Update LoginComponent.vue

--- src/components/Login/LoginComponent.vue
+++ src/components/Login/LoginComponent.vue
@@ -1,4 +1,5 @@
 <template>
+<div style="margin-bottom: 6rem;">
     <div class="login-container">
         <h1 class="text-color outfit">Welcome to EUREKA <br><br></h1>
         <div class="container centerx">
@@ -20,6 +21,7 @@
                 type="gradient">Sign In</vs-button>
         </div>
     </div>
+</div>
 </template>
   
 <script>
