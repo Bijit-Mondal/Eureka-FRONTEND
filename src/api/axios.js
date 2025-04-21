a58e4852d54430f81d8c7e64bf46661b973d6b37
Bijit Mondal <bijitmondal567@gmail.com>
2025-04-21 15:52:37 +0000

    backend changed

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: 'https://backend-eureka.bijit.xyz/',
+  baseURL: 'https://eureka-2025-12b0e27f8a5f.herokuapp.com/',
   // baseURL: 'http://192.168.1.5:3000',
   headers: {
     'Content-Type': 'application/json',
